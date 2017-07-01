771b4cf9b4cd1138442ef5d49f3c811da33dd8f2
tokgozmusa <tokgozmusa@gmail.com>
2017-07-02 02:27:45 +0300

    fix test

--- tests.py
+++ tests.py
@@ -3,4 +3,4 @@ from spell import correction
 def test_correction():
     assert correction("umrella") == "umbrella"
     assert correction("morning") == "morning"
-    assert correction("afteronon") == "afternon"
+    assert correction("afteronon") == "afternoon"
